7b8893ebfdf26aee64f58e6a0f99b998295eb27b
oljakostiv <oljakostivv@gmail.com>
2021-07-20 01:29:55 +0300

    Initial commit

---
+++ ts/main.ts
@@ -0,0 +1,27 @@
+// function numberFn(a: number, b: number) {
+//
+// }
+// numberFn(10, 20);
+//
+// function stringFn (a: string, b:string) {
+//
+// }
+// stringFn('hello', 'world')
+
+function paramFn(
+    str: string,
+    num: number,
+    user: { name: string, age: number },
+    skills: string[],
+    users: { name: string, status: boolean } []
+) {
+
+}
+
+paramFn(
+    'hello',
+    567,
+    {name: 'oleh', age: 28},
+    ['js', 'ts'],
+    [{name: 'nikolai', status: false}, {name: 'taras', status: true}]
+)
